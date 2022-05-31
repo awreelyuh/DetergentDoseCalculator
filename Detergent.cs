290bacf6f9c74c03b61143c2daee441bf30fb1f8
Aurelia Ang Dyson <Aurelia Ang Dyson@AURELIA-PC>
2022-05-31 13:10:29 -0400

    Add project files.

---
+++ Detergent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetergentDoseCalculator
+{
+    public class Detergent
+    {
+        public string? Name { get; set; }
+
+        public float BaseDose { get; set; }
+
+        public Detergent(string? name, float baseDose)
+        {
+            Name = name;
+            BaseDose = baseDose;
+        }
+
+        public void Announce()
+        {
+            Console.WriteLine("Based on a 7.5 kg washing machine capacity, you need " + BaseDose + " scoops/caps of " + Name + " for your main wash.");
+        }
+    }
+}
---
+++ DetergentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetergentDoseCalculator
+{
+    public class DetergentRepository
+    {
+        public static List<Detergent>
+        List<Detergent> detergents = new List<Detergent>();
+            detergents.Add(new Detergent() {"Biozet", 1f});
+            detergents.Add(new Detergent() {"OMO", 1.5f});
+            detergents.Add(new Detergent() {"b clean co", 2f});                 
+        
+    }
+}
