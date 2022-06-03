51c5941dafc3ac8bb3535f962677a0dfce5892e3
Aurelia Ang <findaurelia@live.com>
2022-06-03 00:27:22 -0400

    Fixed List<Detergent> in repository, to match "master"

--- DetergentRepository.cs
+++ DetergentRepository.cs
@@ -8,11 +8,14 @@ namespace DetergentDoseCalculator
 {
     public class DetergentRepository
     {
-        public static List<Detergent>
-        List<Detergent> detergents = new List<Detergent>();
-            detergents.Add(new Detergent() {"Biozet", 1f});
-            detergents.Add(new Detergent() {"OMO", 1.5f});
-            detergents.Add(new Detergent() {"b clean co", 2f});                 
+        public static List<Detergent> GetDetergents()
+        {
+            List<Detergent> detergents = new List<Detergent>();
+            detergents.Add(new Detergent("Biozet", 1f));
+            detergents.Add(new Detergent("OMO", 1.5f));
+            detergents.Add(new Detergent("b clean co", 2f));
+            return detergents;
+        }
         
     }
 }
